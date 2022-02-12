070a01ba554bf5e14454fd2758ef93f2f05f708e
withcolinsong <withcolinsong@gmail.com>
2022-02-12 22:23:22 +0900

    fix: file name

---
+++ java-baseball/src/test/java/study/StringTest.java
@@ -0,0 +1,12 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringTest {
+    @Test
+    void hello() {
+        System.out.println("test");
+    }
+}
