7f47da40691ca520a8bd0b603f6ed835c58d8308
withcolinsong <withcolinsong@gmail.com>
2022-02-16 21:56:04 +0900

    add: split test

--- java-baseball/src/test/java/study/StringTest.java
+++ java-baseball/src/test/java/study/StringTest.java
@@ -6,7 +6,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
     @Test
-    void hello() {
-        System.out.println("test");
+    void substring() {
+        String str = "a,b";
+        assertThat(str.split(",")).contains("b");
+        // 원소 정확히 일치
+        assertThat(str.split(",")).containsExactly("a","b");
+
+        String number = "1";
+        assertThat(number.split(",")).contains(number);
+        assertThat(number.split(",")).containsExactly(number);
+
     }
 }
